f4d740f0d3d60419e9b00600263916eaa35188ae
benice <73033349+SahilRana101@users.noreply.github.com>
2023-06-08 04:40:48 +0530

    last attempt to deploy ig

--- job_board/settings.py
+++ job_board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ot*mv6&s6sf+vfvo-y@v7)+or3i%c$b*pcirw-x#zgig2m!9qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
--- job_board/wsgi.py
+++ job_board/wsgi.py
@@ -3,4 +3,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "job_board.settings")
 application = get_wsgi_application()
 app = application
-
