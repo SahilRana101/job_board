bd39aef2445896396c824778e40c5bff6e798b5f
benice <73033349+SahilRana101@users.noreply.github.com>
2023-06-08 04:06:40 +0530

    Vercel configured

--- job_board/settings.py
+++ job_board/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ot*mv6&s6sf+vfvo-y@v7)+or3i%c$b*pcirw-x#zgig2m!9qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'login_page/templates'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'login_page', 'templates')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
